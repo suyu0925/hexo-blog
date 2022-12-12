8baed321675f6345be808888289381f7f8a674bf
suyu <13936562@qq.com>
2022-12-12 10:51:50 +0800

    更新dist脚本

--- dist.bat
+++ dist.bat
@@ -2,5 +2,6 @@ cd ../suyu0925.github.io
 call git pull
 rmdir /s /q blog
 xcopy ..\hexo-blog\public .\blog /S /I /H /K
+call git add .
 call git commit -a -m "update blog"
 call git push origin master
