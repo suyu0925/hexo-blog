6f2625c4b031272359b36ed5ce836b8f40496a12
suyu <13936562@qq.com>
2024-03-25 21:37:29 +0800

    把suyu0925.github.io/blog导到blog.lckof.com

--- dist.bat
+++ dist.bat
@@ -2,6 +2,7 @@ cd ../suyu0925.github.io
 call git pull
 rmdir /s /q blog
 xcopy ..\hexo-blog\public .\blog /S /I /H /K
+xcopy ..\hexo-blog\assets .\blog /S /I /H /K
 call git add .
 call git commit -a -m "update blog"
 call git push origin master
