36df6e357ab27c0fcd79364efcf4105262db8458
suyu <1393656@qq.com>
2024-07-05 16:51:11 +0800

    添加GA

--- scripts/generate.js
+++ scripts/generate.js
@@ -8,12 +8,23 @@ const bootstrap = async () => {
 
   hexo.extend.injector.register('head_end', () => {
     return `
+<!-- ms clarity -->    
 <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "lbt0m726h2");
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-34Y9Q8PYBQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-34Y9Q8PYBQ');
 </script>
 `
   })
